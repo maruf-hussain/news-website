7227ff40ee2d7636fbdf684f5e0040a708ef5462
maruf-hussain <engmarufhussain@gmail.com>
2023-09-07 14:22:02 +0600

    dsafja

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,14 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBfaJgufDVG1ykXwYmVo5q6fuarU8dEuqk",
-  authDomain: "news-website-965f4.firebaseapp.com",
-  projectId: "news-website-965f4",
-  storageBucket: "news-website-965f4.appspot.com",
-  messagingSenderId: "1040748447377",
-  appId: "1:1040748447377:web:80320e415bf3ec00f3a130"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId=1
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
